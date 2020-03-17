eb8778fc892ea069b6d3c506a0c0aeb34d29e271
Ritiks007 <ritiksaxena007@gmail.com>
2020-03-17 20:17:48 +0530

    Created Game Window

--- Environment.py
+++ Environment.py
@@ -1,2 +1,17 @@
 #TIC TAC TOE Environment
-print("Hello Corona")
+
+import pygame
+
+surface=pygame.display.set_mode((800,600))
+pygame.display.set_caption("tic tac toe")
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    surface.fill([255,255,255])
+    
+    pygame.display.flip()
+pygame.quit()
