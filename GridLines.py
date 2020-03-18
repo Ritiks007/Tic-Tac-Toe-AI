6682ede116c97de134e54f9c32bf6d12fe5423e6
Umme_salma <ummesalma.dairkee@gmail.com>
2020-03-18 12:29:08 +0530

    Added Grid Check

--- GridLines.py
+++ GridLines.py
@@ -2,7 +2,11 @@ import pygame
 from params import Cell_Size,N,Size,grid_color,grid_thickness
 
 class Grid:
+
 	def __init__(self):
+
+		rows, cols = (N, N) 
+		self.CheckGrid = [[1 for i in range(cols)] for j in range(rows)]
 
 		self.gridlines = [((Size, 0),(Size, Size))] # side bar
 
@@ -23,3 +27,9 @@ class Grid:
 	def draw(self, surface):
 		for line in self.gridlines:
 			pygame.draw.line(surface, grid_color, line[0], line[1], grid_thickness)
+
+	def update(self, x, y):
+		self.CheckGrid[x][y] == 0
+
+	def check(self, x, y):
+		return CheckGrid[x][y]
