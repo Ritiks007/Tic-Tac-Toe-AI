b9b5403df01cbf43ba18cacc2b7ce34b5f0812d5
Umme_salma <ummesalma.dairkee@gmail.com>
2020-03-26 20:14:31 +0530

    Added finishing touches to the UI

--- GridLines.py
+++ GridLines.py
@@ -1,11 +1,13 @@
 import pygame
-from params import Cell_Size,N,Size,grid_color,grid_thickness,font
+from params import Cell_Size,N,Size,overall_color,grid_thickness,font, overall_color
 
 class Grid:
+
     def __init__(self):
+        
         self.empty_positions=N*N
         rows, cols = (N, N) 
-        self.CheckGrid = [[-1 for i in range(cols)] for j in range(rows)]
+        self.CheckGrid = [[-1 for i in range(cols)] for j in range(rows)] # Grid to check empty positions and winner
         self.gridlines = [((Size, 0),(Size, Size))] # side bar
 
         # vertical lines
@@ -22,19 +24,23 @@ class Grid:
             self.gridlines.append(Tup)
             i -= 1
 
+    # drawing grid
     def draw(self, surface):
         for line in self.gridlines:
-            pygame.draw.line(surface, grid_color, line[0], line[1], grid_thickness)
-            menu = font.render('MENU', 1, (122, 24, 70))
+            pygame.draw.line(surface, overall_color, line[0], line[1], grid_thickness)
+            menu = font.render('MENU', 1, overall_color)
             surface.blit(menu, (Cell_Size*(N+0.6), 20))
 
+    # updating move in grid
     def update(self, x, y, t):
         self.CheckGrid[x][y] = t+1
         self.empty_positions-=1
 
+    # check for empty or taken positions
     def check(self, x, y):
         return self.CheckGrid[x][y]
 
+    # Checking Win in right, left, up, down, ru, lu, rd, ld directions. Private Functions
     def _right(self, a, b, t):
         if(a == N or self.CheckGrid[a][b] != t):
             return 0
@@ -74,13 +80,17 @@ class Grid:
         if(a == N or b == N or self.CheckGrid[a][b] != t):
             return 0
         return 1 + self._rightDown(a+1, b+1, t)
+    # end of private check functions
 
+    # Check win main function
     def checkwin(self,x,y,t):
         win = []
-        win.append(self._right(x+1, y, t+1) + 1 + self._left(x-1, y, t+1))
-        win.append(self._leftdown(x-1, y+1, t+1) + 1 + self._rightUp(x+1, y-1, t+1))
-        win.append(self._rightDown(x+1, y+1, t+1) + 1 + self._leftUp(x-1, y-1, t+1))
-        win.append(self._up(x, y-1, t+1) + 1 + self._down(x,y+1, t+1))
+        win.append(self._right(x+1, y, t+1) + 1 + self._left(x-1, y, t+1)) # check in horizontal direction
+        win.append(self._leftdown(x-1, y+1, t+1) + 1 + self._rightUp(x+1, y-1, t+1)) # check in one diagnol
+        win.append(self._rightDown(x+1, y+1, t+1) + 1 + self._leftUp(x-1, y-1, t+1)) # check in other diagnol
+        win.append(self._up(x, y-1, t+1) + 1 + self._down(x,y+1, t+1)) # check in vertical direction
+
+        # draw is 3, win of player1 is 1, win of player2 is 2, ongoing game is 0
         if((win[0]<4 and win[1]<4 and win[2]<4 and win[3]<4) and self.empty_positions>0):
             return 0
         elif((win[0]<4 and win[1]<4 and win[2]<4 and win[3]<4) and self.empty_positions==0):
@@ -90,6 +100,7 @@ class Grid:
         elif((win[0]>=4 or win[1]>=4 or win[2]>=4 or win[3]>=4) and t==1):
             return 2
 
+    # New game changes
     def reset(self):
         self.CheckGrid = [[-1 for i in range(N)] for j in range(N)]
         self.empty_positions = N*N
